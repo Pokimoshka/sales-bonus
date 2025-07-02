01e8ce658fd3f8f1fadbf89218db144402ad24ce
DESKTOP-41FTIGS\Егор <g3orgex@yandex.ru>
2025-07-02 17:17:08 +0300

    FIX #1

--- src/main.js
+++ src/main.js
@@ -76,6 +76,8 @@ function analyzeSalesData(data, options) {
         // Увеличить количество продаж 
         seller.sales_count++;
 
+        seller.revenue += record.total_amount;
+
         // Расчёт прибыли для каждого товара
         record.items.forEach(item => {
             const product = productIndex[item.sku]; // Товар
@@ -83,7 +85,6 @@ function analyzeSalesData(data, options) {
             const cost = product.purchase_price * item.quantity;
             // Посчитать выручку (revenue) с учётом скидки через функцию calculateRevenue
             const revenue = calculateRevenue(item, product);
-            seller.revenue += revenue;
             // Посчитать прибыль: выручка минус себестоимость
             const profit = revenue - cost;
             // Увеличить общую накопленную прибыль (profit) у продавца  
@@ -110,12 +111,12 @@ function analyzeSalesData(data, options) {
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
     return sellerStats.map(seller => ({
-            seller_id: seller.id,
-            name: seller.name,
-            revenue: +seller.revenue.toFixed(2),
-            profit: +seller.profit.toFixed(2),
-            sales_count: seller.sales_count,
-            top_products: seller.top_products,
-            bonus: +seller.bonus.toFixed(2)
+        seller_id: seller.id,
+        name: seller.name,
+        revenue: +seller.revenue.toFixed(2),
+        profit: +seller.profit.toFixed(2),
+        sales_count: seller.sales_count,
+        top_products: seller.top_products,
+        bonus: +seller.bonus.toFixed(2)
     })); 
 }
